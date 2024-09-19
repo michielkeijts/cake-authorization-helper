2997bcc1871dcfe8d05bab89ccebfaa8eb7413e2
Michiel Keijts <michiel@normit.nl>
2024-09-19 15:47:20 +0200

    Update Command

--- src/Command/ExtractPolicyActionsCommand.php
+++ src/Command/ExtractPolicyActionsCommand.php
@@ -1,13 +1,13 @@
 <?php
-/* 
- * @copyright (C) 2020 Michiel Keijts, Normit
- * 
+declare(strict_types=1);
+/*
+ * @copyright (C) 2024 Michiel Keijts, Normit
  */
 
 namespace CakeAuthorizationHelper\Command;
 
 use Cake\Console\Arguments;
-use Cake\Console\Command;
+use Cake\Command\Command;
 use Cake\Console\ConsoleIo;
 use DirectoryIterator;
 use RegexIterator;
@@ -16,7 +16,7 @@ use ReflectionClass;
 class ExtractPolicyActionsCommand extends Command
 {
     /**
-     * 
+     *
      * @param Arguments $args
      * @param ConsoleIo $io
      */
@@ -26,24 +26,24 @@ class ExtractPolicyActionsCommand extends Command
         foreach ($this->getPolicies(APP . 'Policy') as $policy) {
             $constants = array_merge($constants, $this->parsePolicy($policy));
         }
-        
+
         $io->out("\"" . implode("\" => TRUE,\n\"", $constants) . "\"");
     }
-    
+
     protected function parsePolicy(string $filepath): array
     {
         if (preg_match("/\/([^\/]+)Policy/i", $filepath, $matches) !== 1) {
             return [];
         }
-        
+
         $name = end($matches);
         $policy_class_name = sprintf("App\\Policy\\%sPolicy", $name);
-        
+
         $constants = [];
         foreach ($this->getConstants($policy_class_name) as $constant) {
             $constants[] = strtoupper($name.'::'.$constant);
         }
-        
+
         return $constants;
     }
 
@@ -56,19 +56,19 @@ class ExtractPolicyActionsCommand extends Command
     protected function getPolicies(string $path = './Policy'): array
     {
         $Directory = new DirectoryIterator($path);
-       
+
         $list = [];
         while ($Directory->valid()) {
             if (strpos($Directory->getFilename(), 'Policy') !== FALSE) {
                 $list[] = $Directory->getPathname();
             }
-            
+
             $Directory->next();
         }
-        
+
         return $list;
     }
-    
+
     /**
      * Get list of constants for a policy
      * @param type $policy
@@ -78,7 +78,7 @@ class ExtractPolicyActionsCommand extends Command
     protected function getConstants($policy, string $prefix = 'ACTION_') : array
     {
         $reflection = new ReflectionClass($policy);
-        
+
         $constants = $reflection->getConstants();
         return array_filter($constants, function ($item, $key) use ($prefix) { return strpos($key, $prefix) === 0; }, ARRAY_FILTER_USE_BOTH);
     }
